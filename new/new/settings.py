9891734874dce41c04e334c28745dd6a1f4be4ce
HJ <72147582+hojaii@users.noreply.github.com>
2022-05-12 14:17:49 +0900

    22.05.12 update

--- new/new/settings.py
+++ new/new/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
     	'ENGINE': 'django.db.backends.mysql',
         'NAME': 'joomo',
         'USER': 'root',
-        'PASSWORD': 'dldnjsgml7',
+        'PASSWORD': '03191121asd!@',
         'HOST': 'localhost',
         'PORT': '3306',
      }
